85233b7e81dbb05d31eee2c169f7033691c4d6ef
mohdruban1 <mohdruban305@gmail.com>
2024-05-28 21:54:46 +0530

    add some file

---
+++ p2.py
@@ -0,0 +1,5 @@
+#WAP to add to nos
+a=10
+b=5
+c=a+b
+print("a+b=",c)
---
+++ p9.py
@@ -0,0 +1,8 @@
+#simple calculator
+a=eval(input("Enter first number="))
+b=eval(input("Enter second number="))
+print("a+b=",a+b)
+print("a-b=",a-b)
+print("a*b=",a*b)
+print("a/b=",a//b)
+print("a%b=",a%b)
